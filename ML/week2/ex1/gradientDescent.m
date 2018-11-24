7ffc9ee78e42fb05bdc26cb61a5d47c5fe450f4f
dengyangjie <295276540@qq.com>
2018-11-24 20:04:18 +0800

    svm

--- ML/week2/ex1/gradientDescent.m
+++ ML/week2/ex1/gradientDescent.m
@@ -17,11 +17,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+
+
     theta = theta - alpha * (X' * ( X*theta - y))/m;   %x'表示x的转置，转置后相乘正好为每一个0要移动的值。
-
-
-
-
 
 
     % ============================================================
--- ML/week2/ex1/gradientDescentMulti.m
+++ ML/week2/ex1/gradientDescentMulti.m
@@ -16,12 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+
+
     delta = (1 / m * (X * theta - y)' * X)'; 
     theta = theta - alpha * delta;           
-
-
-
-
 
 
 
@@ -33,6 +31,6 @@ for iter = 1:num_iters
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
-end
+end;
 
 end
